5f0471f2290233a96b1e6211734d5ec522402995
SHINOBU YOKOTA <7f7fnyam@gmail.com>
2023-05-03 12:44:24 +0900

    プロジェクト ファイルを追加します。

---
+++ ConvertWebp/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace ConvertWebp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chooseBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chooseBtn2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.conv_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // chooseBtn
+            // 
+            this.chooseBtn.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.chooseBtn.Location = new System.Drawing.Point(372, 40);
+            this.chooseBtn.Name = "chooseBtn";
+            this.chooseBtn.Size = new System.Drawing.Size(142, 26);
+            this.chooseBtn.TabIndex = 0;
+            this.chooseBtn.Text = "選択";
+            this.chooseBtn.UseVisualStyleBackColor = true;
+            this.chooseBtn.Click += new System.EventHandler(this.chooseBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F);
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ファイルを選択してください";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(354, 19);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "このファイルでええか？";
+            this.label2.Visible = false;
+            // 
+            // chooseBtn2
+            // 
+            this.chooseBtn2.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.chooseBtn2.Location = new System.Drawing.Point(372, 115);
+            this.chooseBtn2.Name = "chooseBtn2";
+            this.chooseBtn2.Size = new System.Drawing.Size(142, 26);
+            this.chooseBtn2.TabIndex = 5;
+            this.chooseBtn2.Text = "選択";
+            this.chooseBtn2.UseVisualStyleBackColor = true;
+            this.chooseBtn2.Click += new System.EventHandler(this.chooseBtn2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 18F);
+            this.label3.Location = new System.Drawing.Point(12, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(252, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "保存先を指定してください";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 121);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(354, 19);
+            this.textBox2.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // conv_btn
+            // 
+            this.conv_btn.Font = new System.Drawing.Font("MS UI Gothic", 24F);
+            this.conv_btn.Location = new System.Drawing.Point(112, 193);
+            this.conv_btn.Name = "conv_btn";
+            this.conv_btn.Size = new System.Drawing.Size(308, 81);
+            this.conv_btn.TabIndex = 7;
+            this.conv_btn.Text = "じっこう！";
+            this.conv_btn.UseVisualStyleBackColor = true;
+            this.conv_btn.Click += new System.EventHandler(this.conv_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 286);
+            this.Controls.Add(this.conv_btn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chooseBtn2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chooseBtn);
+            this.Name = "Form1";
+            this.Text = "WebPに変換するだけのアプリ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button chooseBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button chooseBtn2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button conv_btn;
+    }
+}
+
---
+++ ConvertWebp/Form1.cs
@@ -0,0 +1,86 @@
+﻿using ImageProcessor.Plugins.WebP.Imaging.Formats;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvertWebp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void chooseBtn_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "画像ファイル|*.png;*.jpg;*.jpeg;*.bmp;*.gif;";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK )
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                label2.Visible = true;
+                Console.WriteLine("FileName: "+openFileDialog1.SafeFileName);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chooseBtn2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog2.Description = "保存先を指定";
+            if(folderBrowserDialog2.ShowDialog() == DialogResult.OK )
+            {
+                textBox2.Text = folderBrowserDialog2.SelectedPath;
+            }
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void conv_btn_Click(object sender, EventArgs e)
+        {
+            if(!(textBox1.Text == "" || textBox2.Text == ""))
+            {
+                DialogResult dr = MessageBox.Show("変換を開始してもよろしいですか？", "確認", MessageBoxButtons.YesNo);
+                if(dr == DialogResult.Yes)
+                {
+                    Console.WriteLine("変換します！");
+                    conv_Img(openFileDialog1, folderBrowserDialog2);
+                }
+                else 
+                { 
+                    Console.WriteLine("変換はキャンセルされたようです！");
+                }
+            }
+            else
+            {
+                Console.WriteLine("ファイルと保存先が指定されていないようです！");
+                MessageBox.Show("ファイルと保存先のどっちかまたは両方が指定されていないようです！","IF文によって実行を止めました");
+            }
+        }
+
+        private void conv_Img(OpenFileDialog filePath, FolderBrowserDialog folderPath)
+        {
+            var wf = new WebPFormat();
+            using (var image = new Bitmap(filePath.FileName))
+            {
+                wf.Save(folderPath.SelectedPath +@"\"+filePath.SafeFileName+".webp", image, 0);
+            }
+
+            MessageBox.Show("変換が終了しました！", "おしらせ");
+        }
+    }
+}
